2bb14944e2a69ed94deb919b7cc2a6ebe99ab45d
Graham Keenan <graham.keenan@outlook.com>
2025-06-20 18:36:19 +0100

    print number of tables

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,58 @@
 use anyhow::{bail, Result};
+use clap::{Parser, Subcommand};
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::{prelude::*, BufReader};
+use std::str::FromStr;
+
+mod sqlite;
+
+#[derive(Debug, Parser)]
+struct Sqlite {
+    /// Name of the Database to load
+    dbname: String,
+
+    /// Command to execute
+    #[arg()]
+    command: SqliteCommand,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum SqliteCommand {
+    /// Display information about the database
+    DbInfo,
+}
+
+impl FromStr for SqliteCommand {
+    type Err = String;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            ".dbinfo" => Ok(SqliteCommand::DbInfo),
+            _ => Err(format!("unknown command: {}", s)),
+        }
+    }
+}
 
 fn main() -> Result<()> {
     // Parse arguments
-    let args = std::env::args().collect::<Vec<_>>();
-    match args.len() {
-        0 | 1 => bail!("Missing <database path> and <command>"),
-        2 => bail!("Missing <command>"),
-        _ => {}
-    }
+    let cli = Sqlite::parse();
+    match cli.command {
+        SqliteCommand::DbInfo => {
+            let db = File::open(cli.dbname)?;
+            let mut reader = BufReader::new(db);
+            let mut header_bytes = [0; 100];
+            reader.read_exact(&mut header_bytes)?;
 
-    // Parse command and act accordingly
-    let command = &args[2];
-    match command.as_str() {
-        ".dbinfo" => {
-            let mut file = File::open(&args[1])?;
-            let mut header = [0; 100];
-            file.read_exact(&mut header)?;
+            let header = sqlite::DatabaseHeader::from(header_bytes);
+            let page_size = header.page_size;
 
-            // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
-            #[allow(unused_variables)]
-            let page_size = u16::from_be_bytes([header[16], header[17]]);
-            println!("database page size: {}", page_size);
+            println!("database page size: {page_size}");
+
+            let mut schema_page = [0; 4096 - 100];
+            reader.read_exact(&mut schema_page)?;
+            let total_tables = u16::from_be_bytes(schema_page[3..5].try_into()?);
+            println!("number of tables: {total_tables}");
         }
-        _ => bail!("Missing or invalid command passed: {}", command),
     }
 
     Ok(())
