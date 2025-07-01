3a906517e7b420c84e29ad8c2e4d3943f7106b0d
Graham Keenan <graham.keenan@outlook.com>
2025-07-01 22:34:37 +0100

    working on sql parser using Nom

--- src/sqlite/sql.rs
+++ src/sqlite/sql.rs
@@ -1,5 +1,5 @@
 use nom::{
-    bytes::complete::{is_not, tag, take_while1},
+    bytes::complete::{is_not, tag, tag_no_case, take_while1},
     character::complete::{char, multispace0},
     combinator::{map, opt},
     multi::separated_list1,
@@ -9,6 +9,7 @@ use nom::{
 
 #[derive(Debug)]
 pub struct SelectStatement {
+    pub operation: Option<Operation>,
     pub columns: Vec<String>,
     pub table: String,
     pub where_clause: Option<Condition>,
@@ -20,9 +21,18 @@ pub struct Condition {
     pub value: String,
 }
 
+#[derive(Debug)]
+pub enum Operation {
+    Count, // For now, only COUNT(*) is supported
+}
+
 fn identifier(input: &str) -> IResult<&str, String> {
-    let (input, ident) = take_while1(|c: char| c.is_alphanumeric() || c == '_')(input)?;
+    let (input, ident) = take_while1(|c: char| c.is_alphanumeric() || c == '_' || c == '*')(input)?;
     Ok((input, ident.to_string()))
+}
+
+fn operation(input: &str) -> IResult<&str, Option<Operation>> {
+    todo!();
 }
 
 fn column_list(input: &str) -> IResult<&str, Vec<String>> {
@@ -48,16 +58,18 @@ fn condition(input: &str) -> IResult<&str, Condition> {
 
 fn where_clause(input: &str) -> IResult<&str, Option<Condition>> {
     opt(preceded(
-        (multispace0, tag("WHERE"), multispace0),
+        (multispace0, tag_no_case("where"), multispace0),
         condition,
     ))
     .parse(input)
 }
 
 pub fn select_statement(input: &str) -> IResult<&str, SelectStatement> {
-    let (input, _) = (tag("SELECT"), multispace0).parse(input)?;
+    let (input, _) = (tag_no_case("select"), multispace0).parse(input)?;
+    // TODO: Fix COUNT(*)
+    // let (input, operation) = operation(input)?;
     let (input, columns) = column_list(input)?;
-    let (input, _) = (multispace0, tag("FROM"), multispace0).parse(input)?;
+    let (input, _) = (multispace0, tag_no_case("from"), multispace0).parse(input)?;
     let (input, table) = identifier(input)?;
     let (input, where_clause) = where_clause(input)?;
     let (input, _) = opt(char(';')).parse(input)?;
@@ -65,6 +77,7 @@ pub fn select_statement(input: &str) -> IResult<&str, SelectStatement> {
     Ok((
         input,
         SelectStatement {
+            operation: None,
             columns,
             table,
             where_clause,
